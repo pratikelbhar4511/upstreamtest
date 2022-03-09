3d41894536e3338a81d5fd81749e1583b71ea3ad
Apurva <apurvamande@logicontech.com>
2022-03-09 23:14:45 +0530

    nine

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
   //res.sendFile(path.join(__dirname, '../', 'public', 'SpeedLossDashboard.html'));
 });
 
-router.get('/getCas2OEE12', function (req, res, next) {
+router.get('/getCas2OEE', function (req, res, next) {
 
 	
 	result= {"recordsets":[[{"Cas2OEE":71.36597321855572}]],"recordset":[{"Cas2OEE":71.36597321855572}],"output":{},"rowsAffected":[1]};
@@ -17,7 +17,7 @@ router.get('/getCas2OEE12', function (req, res, next) {
 
 });
 
-router.get('/login12', function (req, res, next) {
+router.get('/login', function (req, res, next) {
 result={"recordsets":[[{"UserId":4,"Password":"demo","FName":"demo","LName":"user","Email":"demo@mintfdi.com","ContactNo":"12345","Role":"Super Admin","StatusFlag":null,"EmailFlag":null,"SMSFlag":null}]],"recordset":[{"UserId":4,"Password":"demo","FName":"demo","LName":"user","Email":"demo@mintfdi.com","ContactNo":"12345","Role":"Super Admin","StatusFlag":null,"EmailFlag":null,"SMSFlag":null}],"output":{},"rowsAffected":[1]}
 res.send(result);
 
